6139bae39f61417f5c0fb28d2831f978fe783245
bavdin <meranir962@gmail.com>
2026-01-01 21:24:41 +0530

    Update 0_ARIF-GROUP-EMOJI.js

--- models/commands/0_ARIF-GROUP-EMOJI.js
+++ models/commands/0_ARIF-GROUP-EMOJI.js
@@ -13,5 +13,5 @@ module.exports.config = {
 module.exports.run = async function({ api, event, args }) {
 	var emoji = args.join(" ")
 	if (!emoji) api.sendMessage("BOSS SAATH ME EK EMOJI DALO JO LAGANA HA 😐✌️", event.threadID, event.messageID)
-	else api.changeThreadEmoji(emoji, event.threadID, () => api.sendMessage(`BOSS MAINE GROUP KA EMOJI BADAL DIYA 👉 ${emoji}\n━━━━━━━━━━━━━━━━━━━━━━━\nOWNER  𒁍 MR ARIF BABU 🌺`, event.threadID, event.messageID));
+	else api.changeThreadEmoji(emoji, event.threadID, () => api.sendMessage(`BOSS MAINE GROUP KA EMOJI BADAL DIYA 👉 ${emoji}\n━━━━━━━━━━━━━━━━━━━━━━━\nOWNER  𒁍 MR FARAZ BABU 🌺`, event.threadID, event.messageID));
 }
